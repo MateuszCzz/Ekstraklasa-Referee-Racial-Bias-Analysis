5ce8e6d0cabb896e1f2ffe0b55c2ee659fc62507
MateuszCzz <mateuszczernic.mc@gmail.com>
2026-04-19 20:04:04 +0200

    Add startup args for input, output

--- transfermarkt_scraper/__main__.py
+++ transfermarkt_scraper/__main__.py
@@ -4,17 +4,44 @@ from driver import create_driver
 from storage import ensure_data_dir
 from collector import enrich_player_data
 
-BASE_URL = "https://www.google.com/search?q=%22transfermarkt.com%22"
-FORMATTER_INPUT_DIR = Path("data/optaformatter/result")
+BASE_URL = "https://duckduckgo.com/?ia=web&q=%22transfermarkt.com%22"
+# path to results from previous segment
+FORMATTER_INPUT_DIR = Path("data/optaformatter/result") 
+PLAYER_CSV_DIR = FORMATTER_INPUT_DIR / "dimPlayer.csv"
+# path to store results 
 DATA_DIR = Path("data/transfermarktscraper")
 RESULT_DIR = DATA_DIR / "result"
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--headless", action="store_true", default=False, help="Run browser in headless mode")
+    parser.add_argument(
+        "--headless",
+        action="store_true",
+        default=False,
+        help="Run browser in headless mode")
+    
+    # arg in
+    parser.add_argument(
+        "--data-in", 
+        type=Path, 
+        default=PLAYER_CSV_DIR, 
+        metavar="DIR",
+        help=f"Directory containing dimPlayer.csv (default: {PLAYER_CSV_DIR})",
+        )
+    
+    #arg out
+    parser.add_argument(
+        "--data-out",
+        type=Path,
+        default=RESULT_DIR,
+        metavar="DIR",
+        help="Where to write results to (default: {RESULT_DIR})",
+    )
     args = parser.parse_args()
 
-    ensure_data_dir(DATA_DIR)
+    output_dir = args.data_out or RESULT_DIR
+    ensure_data_dir(output_dir)
+
     driver = create_driver(headless=args.headless)
     try:
         enrich_player_data(driver, BASE_URL)
