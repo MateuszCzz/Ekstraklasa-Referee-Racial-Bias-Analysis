020b2d22e4e4dc999a2110cc705f351de3e04df9
MateuszCzz <mateuszczernic.mc@gmail.com>
2026-05-09 23:30:35 +0200

    Add dismissal for cookies prompt

--- transfermarkt_scraper/parser.py
+++ transfermarkt_scraper/parser.py
@@ -4,12 +4,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 QUERY_RESULT_CSS = "li[data-layout='organic'] h2 a"
+COOKIES_ACCEPT_BUTTON_CSS = ".accept-all"
+CONSENT_IFRAME_CSS = "iframe[id^='sp_message_iframe']"
 
 ELEMENT_LOAD_WAIT = 10 # delay for elements to become visible
+
+def _dismiss_cookie_prompt(driver) -> None:
+    # # cookies already cleared once
+    # if getattr(driver, "_cookies_accepted", False):
+    #     return
+    try:
+        # switch into consent iframe
+        iframe = WebDriverWait(driver, ELEMENT_LOAD_WAIT).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, CONSENT_IFRAME_CSS))
+        )
+        driver.switch_to.frame(iframe)
+
+        # get button
+        WebDriverWait(driver, ELEMENT_LOAD_WAIT).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, COOKIES_ACCEPT_BUTTON_CSS))
+        ).click()
+
+        # # set flag in driver
+        # driver._cookies_accepted = True
+
+    except Exception as e:
+        print(f" [parser] Failed to dismiss cookies prompt, safe to ignore: {e}")
+        pass
+
+    finally:
+        # switch back
+        driver.switch_to.default_content()  
 
 def parse_player_page(driver) -> dict:
     """Gets player data from given transfermarkt page."""
     data: dict = {}
+
+    # dismiss cookies prompt
+    _dismiss_cookie_prompt(driver)
 
     return data
 
